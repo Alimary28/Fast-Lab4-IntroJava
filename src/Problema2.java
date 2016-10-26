ffa6a27b442ea142ce4531d58e123ff811c4a907
Alimary 28 <alexandru28.pa@gmail.com>
2016-10-26 16:20:45 +0300

    Calcularea sumei numerelor pana la 9.

---
+++ src/Problema2.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Vaio on 26.10.2016.
+ */
+public class Problema2 {
+
+    public static void main(String[] args) {
+
+        int[] n = {1,2,3,4,5,6,7,8,9};
+        int sum = 0;
+
+        for (int num : n){
+
+            sum = sum + num ;
+        }
+        System.out.println("Suma numerelor este:" +sum);
+    }
+}
