b210a9a93dc611903263cbedc2556f251d364b79
Alimary 28 <alexandru28.pa@gmail.com>
2016-10-27 23:17:33 +0300

    Calculare numar ridicat la puterea altui numar.

---
+++ src/PowerOf.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Vaio on 27.10.2016.
+ */
+public class PowerOf {
+    public static void main(String[] args) {
+
+        int a = 4 ;
+        int n = 6 ;
+        int i =1;
+        int rezultat=0;
+
+        for (i = 1; i<n;i ++ ); {
+            rezultat= a*a*a*a*a*a;
+
+
+        }
+        System.out.println("Rezultatul este :"+ rezultat );
+
+    }
+}
